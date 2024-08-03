588c23cfb9d57af55760c55766b353cfb27a8356
Mohiduzzaman Mahin <mahin.swe.14@gmail.com>
2024-08-03 12:32:19 +0600

    Create Concatenation of Array.cpp

---
+++ Concatenation of Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            v.push_back(nums[i]);
+        }
+        return v;
+    }
+};
